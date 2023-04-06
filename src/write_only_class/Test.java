3de8fd2435ebe730d3e36c8d651dd21bfd1d3551
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-06 15:04:34 +0700

    Create Test.java

---
+++ src/write_only_class/Test.java
@@ -0,0 +1,11 @@
+package write_only_class;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        Student student2 = new Student();
+        student1.setName("Thomas Shelby");
+        student2.setName("Chung pHung Phat");
+        student2.setClasses("C0223H1");
+    }
+}
