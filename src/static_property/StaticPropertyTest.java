0fee2eadb13e32ce2b9f913ed85d718ad8d62d2a
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-06 14:39:06 +0700

    Create StaticPropertyTest.java

---
+++ src/static_property/StaticPropertyTest.java
@@ -0,0 +1,14 @@
+package static_property;
+
+public class StaticPropertyTest {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+
+        System.out.println(Car.numberOfCars);
+
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+        Car car3 = new Car("Mazda 7","Skyactiv 6");
+
+        System.out.println(Car.numberOfCars);
+    }
+}
